fd6d4474975a6ea592954943784849b795c1611d
Michilis <120072772+Michilis@users.noreply.github.com>
2023-08-18 18:43:02 +0200

    Update lightningcheckout.php

--- lightningcheckout.php
+++ lightningcheckout.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
-Plugin Name: Bitcoin Payment Gateway (lightning)
+Plugin Naam: Bitcoin Betalingsgateway (Lightning)
 Plugin URI: https://lightningcheckout.eu
-Description: Accept Bitcoin over the lightning network. Brought to you by Lightning Checkout
-Version: 2.0
-Author: Lightning Checkout
-Fork of: https://nl.wordpress.org/plugins/lightning-payment-gateway-lnbits/
+Omschrijving: Accepteer Bitcoin via het Lightning-netwerk. Aangeboden door Lightning Checkout.
+Versie: 2.0
+Auteur: Lightning Checkout
+Fork van: https://nl.wordpress.org/plugins/lightning-payment-gateway-lnbits/
 */
 
 require 'plugin-update-checker/plugin-update-checker.php';
@@ -18,14 +18,13 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 	'wp-lnc-bitcoinlightning'
 );
 
-
-//Set the branch that contains the stable release.
+// Stel de tak in die de stabiele release bevat.
 $myUpdateChecker->setBranch('main');
 
-//Optional: If you're using a private repository, specify the access token like this:
-//$myUpdateChecker->setAuthentication('your-token-here');
+// Optioneel: als je een privérepository gebruikt, specificeer dan de toegangstoken als volgt:
+//$myUpdateChecker->setAuthentication('jouw-token-hier');
 
-//If you want to use release assets, call the enableReleaseAssets() method after creating the update checker instance:
+// Als je release-assets wilt gebruiken, roep dan de enableReleaseAssets() methode aan nadat je de update checker-instantie hebt gemaakt:
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
 
 
@@ -41,7 +40,7 @@ use LightningCheckoutPlugin\Utils;
 use LightningCheckoutPlugin\LNBitsAPI;
 
 
-function woocommerce_lightningcheckout_activate() {
+function pmpro_lightningcheckout_activate() {
     if (!current_user_can('activate_plugins')) return;
 
     global $wpdb;
@@ -56,15 +55,15 @@ function woocommerce_lightningcheckout_activate() {
           'post_content' => render_template('payment_page.php', array())
         );
 
-        // insert the post into the database
+        // voeg de post toe aan de database
         wp_insert_post( $page );
     }
 }
 
-register_activation_hook(__FILE__, 'woocommerce_lightningcheckout_activate');
+register_activation_hook(__FILE__, 'pmpro_lightningcheckout_activate');
 
 
-// Helper to render templates under ./templates.
+// Helper om sjablonen te renderen onder ./templates.
 function render_template($tpl_name, $params) {
     return wc_get_template_html($tpl_name, $params, '', plugin_dir_path(__FILE__).'templates/');
 }
@@ -75,8 +74,7 @@ function qr_code_load(){
   wp_enqueue_script( 'qr-code', plugin_dir_url( __FILE__ ) . 'js/jquery.qrcode.min.js', array( 'jquery' ) );
 }
 
-
-// Generate lightningcheckout_payment page, using ./templates/lightningcheckout_payment.php
+// Genereer lightningcheckout_payment-pagina, met behulp van ./templates/lightningcheckout_payment.php
 function lightningcheckout_payment_shortcode() {
     $check_payment_url = trailingslashit(get_bloginfo('wpurl')) . '?wc-api=wc_gateway_lightningcheckout';
 
@@ -86,7 +84,7 @@ function lightningcheckout_payment_shortcode() {
         $invoice = $order->get_meta("lightningcheckout_invoice");
         $success_url = $order->get_checkout_order_received_url();
     } else {
-        // Likely when editting page with this shortcode, use dummy order.
+        // Waarschijnlijk bij bewerken van pagina met deze shortcode, gebruik een dummy-bestelling.
         $order_id = 1;
         $invoice = "lnbc0000";
         $success_url = "/dummy-success";
@@ -102,175 +100,53 @@ function lightningcheckout_payment_shortcode() {
     return render_template('payment_shortcode.php', $template_params);
 }
 
-
-
-// This is the entry point of the plugin, where everything is registered/hooked up into WordPress.
+// Dit is het startpunt van de plugin, waar alles in WordPress wordt geregistreerd/gekoppeld.
 function lightningcheckout_init() {
-    if (!class_exists('WC_Payment_Gateway')) {
+    if (!class_exists('PMProGateway')) {
         return;
     };
 
-    // Register shortcode for rendering Lightning invoice (QR code)
+    // Registreer shortcode voor het weergeven van Lightning-invoice (QR-code)
     add_shortcode('lightningcheckout_payment_shortcode', 'lightningcheckout_payment_shortcode');
 
-    // Register the gateway, essentially a controller that handles all requests.
-    function add_lightningcheckout_gateway($methods) {
-        $methods[] = 'WC_Gateway_LNBits';
-        return $methods;
+    // Registreer de gateway, feitelijk een controller die alle verzoeken afhandelt.
+    function add_lightningcheckout_gateway($gateways) {
+        $gateways['lightningcheckout'] = 'PMProGateway_LNBits';
+        return $gateways;
     }
 
-    add_filter('woocommerce_payment_gateways', 'add_lightningcheckout_gateway');
+    add_filter('pmpro_gateways', 'add_lightningcheckout_gateway');
 
-
-    // Defined here, because it needs to be defined after WC_Payment_Gateway is already loaded.
-    class WC_Gateway_LNBits extends WC_Payment_Gateway {
+    // Definieer hier, omdat het moet worden gedefinieerd nadat PMProGateway al is geladen.
+    class PMProGateway_LNBits extends PMProGateway {
         public function __construct() {
-            global $woocommerce;
+            global $wpdb;
 
-            $this->id = 'lightningcheckout';
-            $this->icon = plugin_dir_url(__FILE__).'assets/lightning.png';
-            $this->has_fields = false;
-            $this->method_title = 'Bitcoin Lightning';
-            $this->method_description = 'Accept bitcoin lightning payments via Lightning Checkout.';
+            $this->gateway = 'lightningcheckout';
+            $this->gateway_environment = pmpro_getOption('gateway_environment');
+            $this->gateway_options = pmpro_getOption('gateway_options');
 
-            $this->init_form_fields();
-            $this->init_settings();
+            $this->liveurl = 'https://pay.lightningcheckout.eu';
+            $this->testurl = 'https://pay.lightningcheckout.eu'; // Gebruik dezelfde URL voor tests omdat het een sandbox-omgeving is
+            $this->api_key = $this->get_option('lightningcheckout_api_key');
 
-            $this->title = 'Bitcoin Lightning';
-            $this->description = '';
+            add_action('init', array($this, 'load_textdomain'));
 
-            $url = 'https://pay.lightningcheckout.eu';
-            $api_key = $this->get_option('lightningcheckout_api_key');
-            $this->api = new LNBitsAPI($url, $api_key);
-
-            add_action('woocommerce_update_options_payment_gateways_'.$this->id, array($this, 'process_admin_options'));
-            add_action('woocommerce_thankyou_'.$this->id, array($this, 'thankyou'));
-            add_action('woocommerce_api_wc_gateway_'.$this->id, array($this, 'check_payment'));
-        }
-
-        /**
-         * Render admin options/settings.
-         */
-        public function admin_options() {
-            ?>
-            <h3><?php _e('Lightning Checkout', 'woothemes'); ?></h3>
-            <p><?php _e('Accept Bitcoin instantly through the Lightning network.', 'woothemes'); ?></p>
-            <table class="form-table">
-                <?php $this->generate_settings_html(); ?>
-            </table>
-            <?php
-
-        }
-
-        /**
-         * Generate config form fields, shown in admin->WooCommerce->Settings.
-         */
-        public function init_form_fields() {
-            // echo("init_form_fields");
-            $this->form_fields = array(
-                'enabled' => array(
-                    'title' => __('Lightning Checkout', 'woocommerce'),
-                    'label' => __('Enable bitcoin payments via the Lightning network', 'woocommerce'),
-                    'type' => 'checkbox',
-                    'description' => '',
-                    'default' => 'no',
-                ),
-                'lightningcheckout_api_key' => array(
-                    'title' => __('API Key', 'woocommerce'),
-                    'type' => 'text',
-                    'description' => __(get_bloginfo() . ', Enter your personal API key. Get one via https://lightningcheckout.eu/en/contact/', 'woocommerce'),
-                    'default' => '',
-                ),
-            );
-        }
-
-
-        /**
-         * ? Output for thank you page.
-         */
-        public function thankyou() {
-            if ($description = $this->get_description()) {
-                echo esc_html(wpautop(wptexturize($description)));
+            if($this->api_key) {
+                add_action('pmpro_checkout_preheader', array($this, 'process_payment'));
             }
-        }
 
+            add_filter('pmpro_payment_option_fields', array($this, 'payment_option_fields'), 10, 2);
+            add_filter('pmpro_payment_option_fields', array($this, 'pmpro_payment_option_fields'), 10, 2);
+            add_filter('pmpro_checkout_order', array($this, 'pmpro_checkout_order'));
+            add_action('wp_ajax_pmpro_lightningcheckout', array($this, 'pmpro_lightningcheckout'));
+            add_action('wp_ajax_nopriv_pmpro_lightningcheckout', array($this, 'pmpro_lightningcheckout'));
 
-        /**
-         * Called from checkout page, when "Place order" hit, through AJAX.
-         * 
-         * Call LNBits API to create an invoice, and store the invoice in the order metadata.
-         */
-        public function process_payment($order_id) {
-            $order = wc_get_order($order_id);
-            // This will be stored in the Lightning invoice (ie. can be used to match orders in LNBits)
+            add_action('pmpro_cron_expiration_warning', array($this, 'pmpro_cron_expiration_warning'));
+            add_action('pmpro_cron_expire_memberships', array($this, 'pmpro_cron_expire_memberships'));
+            add_action('pmpro_cron_credit_card_expiring', array($this, 'pmpro_cron_credit_card_expiring'));
+            add_action('pmpro_cron_jobs', array($this, 'pmpro_cron_jobs'));
 
-            $memo = SITE_NAME." order ".$order->get_id()." (".$order->get_total() ." ".get_woocommerce_currency().")";
-
-            $amount = Utils::convert_to_satoshis($order->get_total(), get_woocommerce_currency());
-
-            $r = $this->api->createInvoice($amount, $memo);
-
-            if ($r['status'] == 201) {
-                $resp = $r['response'];
-                $order->add_meta_data('lightningcheckout_invoice', $resp['payment_request'], true);
-                $order->add_meta_data('lightningcheckout_payment_hash', $resp['payment_hash'], true);
-                $order->save();
-
-                // TODO: configurable payment page slug
-                $redirect_url = add_query_arg(array("order_id" => $order->get_id()), get_permalink( get_page_by_path( LIGHTNINGCHECKOUT_PAYMENT_PAGE_SLUG ) ));
-
-                return array(
-                    "result" => "success",
-                    "redirect" => $redirect_url
-                );
-            } else {
-                error_log("Lightning Checkout API failure. Status=".$r['status']);
-                error_log($r['response']);
-                return array(
-                    "result" => "failure",
-                    "messages" => array("Failed to create Lightning invoice.")
-                );
-            }
-        }
-
-
-        /**
-         * Called by lightningcheckout_payment page (with QR code), through ajax.
-         * 
-         * Checks whether given invoice was paid, using LNBits API,
-         * and updates order metadata in the database.
-         */
-        public function check_payment() {
-            $order = wc_get_order($_REQUEST['order_id']);
-            $payment_hash = $order->get_meta('lightningcheckout_payment_hash');
-            $r = $this->api->checkInvoicePaid($payment_hash);
-
-            if ($r['status'] == 200) {
-                if ($r['response']['paid'] == true) {
-                    $order->add_order_note('Payment is settled and added to your Lightning Checkout balance.');
-                    $order->payment_complete();
-                    $order->save();
-                    error_log("PAID");
-                    echo(json_encode(array(
-                        'result' => 'success',
-                        'redirect' => $order->get_checkout_order_received_url(),
-                        'paid' => true
-                    )));
-                } else {
-                    echo(json_encode(array(
-                        'result' => 'success',
-                        'paid' => false
-                    )));
-                }
-            } else {
-                echo(json_encode(array(
-                    'result' => 'failure',
-                    'paid' => false,
-                    'messages' => array('Request to payment provider failed.')
-                )));
-
-            }
-            die();
-        }
-    }
-}
+            // adds the settings to the Payment Settings meta box
+            add_filter('pmpro_payment_options', array($this, 'pmpro_payment_options'));
+            // updates to our custom
